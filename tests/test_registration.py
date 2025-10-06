c1e830a2ca8d014a317cf8a5e6c1efb698857148
timoshk <tzzelensk@gmail.com>
2025-10-06 17:35:06 +0500

    Интеграция Playwright and Pytest

---
+++ tests/test_registration.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import sync_playwright
+
+def test_succesful_registration():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+        email = page.get_by_test_id('registration-form-email-input').locator('input')
+        email.fill('timowka@mail.ru')
+        username = page.get_by_test_id('registration-form-username-input').locator('input')
+        username.fill('teswt1')
+        password = page.get_by_test_id('registration-form-password-input').locator('input')
+        password.fill('123123')
+        reg = page.get_by_test_id('registration-page-registration-button')
+        reg.click()
+
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+
+        # page.wait_for_timeout(3008)
+        print('Succesful registration')
