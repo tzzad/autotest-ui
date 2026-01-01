9c293a417e4ad6adc36f1de65b0de15bc4d16263
tzzelen <tzzelensk@gmail.com>
2026-01-01 19:26:18 +0500

    Yes

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -9,3 +9,10 @@ def send_analytics_data():
 @pytest.fixture(scope="session")
 def settings():
     ...
+
+@pytest.fixture(scope="class")
+def user():
+    ...
+@pytest.fixture(scope="function")
+def browser():
+    ...
