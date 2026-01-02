ebb208fdbd8964b93dbad621f23cde43ae06d4e9
tzzelen <tzzelensk@gmail.com>
2026-01-02 17:17:29 +0500

    fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
 
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright, Page,Playwright
 
 @pytest.fixture
-def chromium_page() -> Page:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        yield browser.new_page()
-        browser.close()
+def chromium_page(playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,17 +6,13 @@ from playwright.sync_api import Page, expect
 def test_succesful_registration(chromium_page: Page):
         chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-        email = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email.fill('timowka@mail.ru')
+        chromium_page.get_by_test_id('registration-form-email-input').locator('input').fill('timowka@mail.ru')
 
-        username = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username.fill('teswt1')
+        chromium_page.get_by_test_id('registration-form-username-input').locator('input').fill('teswt1')
 
-        password = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password.fill('123123')
+        chromium_page.get_by_test_id('registration-form-password-input').locator('input').fill('123123')
 
-        reg = chromium_page.get_by_test_id('registration-page-registration-button')
-        reg.click()
+        chromium_page.get_by_test_id('registration-page-registration-button').click()
 
         dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
         expect(dashboard_title).to_be_visible()
