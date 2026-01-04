53ae1fe73c7423f9bf6e4f16f7f4d106f26544b0
tzzelen <tzzelensk@gmail.com>
2026-01-04 12:26:33 +0500

    Change

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,18 +1,17 @@
-from symtable import Class
+from typing import Any, Generator
 
 import pytest
-from playwright.sync_api import Page
 
 
 @pytest.fixture
-def chromium_page(playwright) -> Page:
+def chromium_page(playwright) -> Generator[Any, Any, None]:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
 
 
 @pytest.fixture(scope="session")
-def initialize_browser_state(playwright, Playwright):
+def initialize_browser_state(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -34,7 +33,7 @@ def initialize_browser_state(playwright, Playwright):
     context.storage_state(path="browser-state.json")
 
 @pytest.fixture(scope="function")
-def chromium_page_with_state(initialize_browser_state, playwright, Playwright) -> Page:
+def chromium_page_with_state(initialize_browser_state, playwright, chromium_page) -> Generator[Any, Any, None]:
     browser = playwright.chromium.launch(headless=False)
     context = chromium_page().new_context(storage_state='browser-state.json')
     yield context.new_page()
