458b68878902a66b1fd8d96729f509286eb5377e
tzzelen <tzzelensk@gmail.com>
2026-01-05 18:12:28 +0500

    Change

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
 pytest_plugins = [
-    "fixtures.browsers"
+    "fixtures.browsers",
+    "fixtures.pages"
 ]
 
 # сюда прописываются все файлы с фикстурами, чтобы использовать их во всех папках
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,8 @@
 import pytest
 from playwright.sync_api import expect, Page
-
+from pages.login_page import LoginPage
 
 @pytest.mark.regression
-@pytest.mark.authorization
 @pytest.mark.parametrize(
     "email, password",
     [
@@ -12,28 +11,8 @@ from playwright.sync_api import expect, Page
         ("  ", "password")
     ]
 )
-def test_wrong_email_or_password(chromium_page: Page, email: str, password: str):
-        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")  # страница куда зайти
-
-        disable = chromium_page.get_by_test_id('login-page-login-button')
-        expect(disable).to_be_disabled()  # проверяем, что до ввода данных кнопка не активна
-
-        email = chromium_page.get_by_test_id('login-form-email-input').locator('input')  # локатор куда заполнять
-        email.fill('email')  # Филом заполнил этими данными
-
-        password = chromium_page.locator('[data-testid="login-form-password-input"] input')
-        password.fill('password')
-
-        login = chromium_page.get_by_test_id('login-page-login-button')
-        login.click()
-
-        alert = chromium_page.locator('[data-testid="login-page-wrong-email-or-password-alert"]')
-        expect(alert).to_be_visible()  # проверка, что локатор видим/не видим visible - виден, hidden - не виден
-        expect(alert).to_have_text('Wrong email or password')  # проверка, что содержится текст который нам необходим
-
-        reglink = chromium_page.get_by_test_id('login-page-registration-link')
-        reglink.hover()  # ховер это наведение мыши чтоб проверить активность
-        chromium_page.wait_for_timeout(2050) # ожидание 2с для демо проверки, что рил выполняются действия в браузере, в проектах не используется
-
-
-        print('✅Тест пройден✅')
+def test_wrong_email_or_password(login_page: LoginPage, email: str, password: str):
+        login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+        login_page.fill_login_form(email=email, password=password)
+        login_page.click_login_button()
+        login_page.check_wrong_email_or_password_alert()
